968424a5d0f3c730887db8c202d7cade72d65fc7
Spencer Dent <sd00070@my.westga.edu>
2021-03-26 13:39:24 -0400

    improve documentation

--- util/Util.php
+++ util/Util.php
@@ -1,17 +1,25 @@
 <?php
 class Util
 {
-    /****************************************************************
-     * Gets the path
-     ***************************************************************/
+    /**
+     * Get the root location of the project.
+     * 
+     * @return project_path
+     */
     public static function getProjectPath()
     {
         return "/cs6252/projects/project2_spencerDent";
     }
 
-    /****************************************************************
-     * Gets the action
-     ***************************************************************/
+    /**
+     * Retrieves the action from the client's request.
+     * It checks if the old request is still active first, then checks POST,
+     * and finally it checks GET.
+     * 
+     * If it is unable to find the action, it returns an empty action String.
+     * 
+     * @return user_action
+     */
     public static function getAction($old_action = '')
     {
         if ($old_action !== '') {
